e53b393d4304b83efd4bc8b4f827c361853f7592
Karanpal Sekhon <ksekhon2015@gmail.com>
2021-02-13 14:23:19 -0700

    updated user class

--- User.py
+++ User.py
@@ -3,12 +3,14 @@ class User:
     def __init__(self):
         self
 
-    def __init__(self, ID, passcode, major, interests):
+    def __init__(self, ID, name, passcode, major, interests, club, media_account, classes):
         self.id = ID
+        self.name = name
         self.passcode = passcode
         self.major = major
         self.interests = interests
 
+    
 
 
 
