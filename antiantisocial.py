8454cc483ad11490f74055decf4d6c1916d32bd4
Alec Zaiane <50636059+alec-zaiane@users.noreply.github.com>
2021-02-13 15:46:04 -0700

    Import dummy names

--- antiantisocial.py
+++ antiantisocial.py
@@ -1,9 +1,15 @@
 
-
-if __name__ == "__main__":
+def importDummyNames():
     from User import User
     student = []
     with open("DummyNames.csv", "r") as f:
         for line in f.readlines():
             s = User(line)
             student.append(s)
+    return student
+
+
+if __name__ == "__main__":
+    students = importDummyNames()
+
+
