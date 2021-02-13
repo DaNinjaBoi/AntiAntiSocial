ea8f2cf450f6cd94bf8d3adace24b3df6f8c837e
Karanpal Sekhon <ksekhon2015@gmail.com>
2021-02-13 14:29:22 -0700

    updated user class

--- User.py
+++ User.py
@@ -1,8 +1,7 @@
+from typing import Any
+
 
 class User:
-    def __init__(self):
-        self
-
     def __init__(self, ID, name, passcode, major, interests, club, media_account, classes):
         self.id = ID
         self.name = name
@@ -10,7 +9,26 @@ class User:
         self.major = major
         self.interests = interests
 
-    
+    def __init__(self):
+        pass
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def get_major(self):
+        return self.major
+
+    def get_interests(self):
+        return self.interests
+
+
+
+
+
+
 
 
 
