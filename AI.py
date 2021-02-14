404dd51036de294d25d6f6297279f3d04ea970b1
Alec Zaiane <50636059+alec-zaiane@users.noreply.github.com>
2021-02-13 19:21:52 -0700

    Started AI preprocessing

--- AI.py
+++ AI.py
@@ -1,9 +1,28 @@
 import antiantisocial
+import User
 
-stud = antiantisocial.importDummyNames()
+def preprocess(user1, user2):
+    """
+    Converts Two users into an array of numbers for use by the AI
+    :param user1: First user
+    :param user2: Second user
+    :return: Array of numbers to pass into AI
+    """
+    numList = []
+    if user1.get_major() == user2.get_major():
+        numList.append(1)
+    else:
+        numList.append(0)
+    for i in user1.get_interests():
+        if i:
+            numList.append(1)
+        else:
+            numList.append(0)
+    for i in user2.get_interests():
+        if i:
+            numList.append(1)
+        else:
+            numList.append(0)
 
-def generate_reccomendations(students, index):
-    user = students[index]
 
-generate_reccomendations(stud,0)
 
