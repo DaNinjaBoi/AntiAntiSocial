d4744244be61c73f6792955c9c17f56ac3e310da
Karanpal Sekhon <ksekhon2015@gmail.com>
2021-02-13 20:52:32 -0700

    updated user class

--- antiantisocial.py
+++ antiantisocial.py
@@ -4,7 +4,6 @@ import AI
 
 
 
-
 if __name__ == "__main__":
     # generateDummyNames.generate_dummy_names()
     students = AAS_IO.importDummyNames()
---
+++ manual_trainer.py
@@ -0,0 +1,13 @@
+import User
+from AAS_IO import importDummyNames
+student = importDummyNames()
+
+student[0].print_user()
+
+def manual_train():
+    pass
+
+def display_student():
+    pass
+
+# add telling training method y or n on two specific people
